0ad7de07f11b3dbc1f621b1018fd0190d060c65f
Obunde <eugeneokumu01@gmail.com>
2025-02-20 08:39:25 +0300

    tests and validations

--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -1,48 +1,69 @@
 require "test_helper"
 
 class BooksControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @book = Book.create(title: "Test Book", author: "Test Author", isbn: "12345")
+  end
+
   test "should get index" do
-    get books_index_url
+    get books_path
     assert_response :success
   end
 
   test "should get show" do
-    get books_show_url
+    get book_path(@book)
     assert_response :success
   end
 
   test "should get new" do
-    get books_new_url
+    get new_book_path
     assert_response :success
   end
 
-  test "should get create" do
-    get books_create_url
-    assert_response :success
+  test "should create book" do
+    assert_difference("Book.count", 1) do
+      post books_path, params: { book: { title: "New Book", author: "New Author", isbn: "67890" } }
+    end
+    assert_redirected_to book_path(Book.last)
+    follow_redirect!
+    assert_match "Book was successfully created.", response.body
   end
 
   test "should get edit" do
-    get books_edit_url
+    get edit_book_path(@book)
     assert_response :success
   end
 
-  test "should get update" do
-    get books_update_url
-    assert_response :success
+  test "should update book" do
+    patch book_path(@book), params: { book: { title: "Updated Title" } }
+    assert_redirected_to book_path(@book)
+    @book.reload
+    assert_equal "Updated Title", @book.title
   end
 
-  test "should get destroy" do
-    get books_destroy_url
-    assert_response :success
+  test "should destroy book" do
+    assert_difference("Book.count", -1) do
+      delete book_path(@book)
+    end
+    assert_redirected_to books_path
+    follow_redirect!
+    assert_match "Book was successfully deleted.", response.body
   end
 
-  test "should get borrow" do
-    get books_borrow_url
-    assert_response :success
+  test "should borrow book" do
+    borrower = Borrower.create(name: "John Doe")
+    post borrow_book_path(@book), params: { borrower_name: borrower.name }
+    assert_redirected_to book_path(@book)
+    @book.reload
+    assert_not @book.is_available
   end
 
-  test "should get return" do
-    get books_return_url
-    assert_response :success
+  test "should return book" do
+    borrower = Borrower.create(name: "John Doe")
+    @book.borrow(borrower)
+    post return_book_path(@book)
+    assert_redirected_to book_path(@book)
+    @book.reload
+    assert @book.is_available
   end
 end
--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -1,7 +1,22 @@
 require "test_helper"
 
 class BookTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @book = Book.new(title: "Test Book", author: "Test Author", isbn: "12345")
+  end
+
+  test "should be valid with valid attributes" do
+    assert @book.valid?
+  end
+
+  test "should not be valid without a title" do
+    @book.title = nil
+    assert_not @book.valid?
+  end
+
+  test "should mark book as unavailable when borrowed" do
+    borrower = Borrower.create(name: "John Doe")
+    @book.borrow(borrower)
+    assert_not @book.is_available
+  end
 end
